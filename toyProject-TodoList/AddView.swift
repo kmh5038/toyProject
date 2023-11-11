52b59110d0654db49a9207ff783bf3c6b2506f1f
kmh5038 <144766297+kmh5038@users.noreply.github.com>
2023-11-11 14:02:42 +0900

    AddView complete , List button
    AddView complete , List button

--- toyProject-TodoList/AddView.swift
+++ toyProject-TodoList/AddView.swift
@@ -8,15 +8,15 @@
 import SwiftUI
 
 struct AddView: View {
-    
+    @Binding var todoData: [Todo]
     @State private var titleTextField = ""
     @State private var scheduleTextEditor = ""
+    @Environment(\.dismiss) var dismiss // Environment 프로퍼티 래퍼 정확히 무슨 기능인지 모르겠음
     
     
     var body: some View {
         NavigationStack {
             VStack {
-                
                 Text("Title ")
                     .font(.headline)
                     
@@ -26,35 +26,39 @@ struct AddView: View {
                     .overlay(RoundedRectangle(cornerRadius: 6)
                         .stroke(Color.black, lineWidth: 1))
                     .padding()
-                    
                 Text("Schedule")
                     .font(.headline)
-                    
-                
                 TextEditor(text: $scheduleTextEditor)
                     .overlay(RoundedRectangle(cornerRadius: 8)
                         .stroke(Color.black, lineWidth: 1))
                     .padding()
                 Spacer(minLength: 30)
-                Button("ADD") {
-                   
-                }
                 
+                Button("ADD") {
+                    let newTodo = Todo(id: getNextID(), title: titleTextField, description: scheduleTextEditor, completed: false)
+                    todoData.append(newTodo) // id는 함수를 만들어 넣어주고 title에 TextField에 입력한 값, description애 TextEditor에 입력한값 completed는 false
+                    dismiss() // 새 스케줄을 만들어주고 ContentView로 이동
+                }
                 .font(.title2)
                 .kerning(1)
                 .disabled(titleTextField.isEmpty || scheduleTextEditor.isEmpty ? true : false)
             }
-            
-          
         }
         .navigationTitle("Add Schedule")
         
             
     }
     
+    private func getNextID() -> Int { // 새로 만들어질 List 요소 id 값 생성해주는 함수 ( 마지막 요소 id 값에 + 1 을 해준다)
+        if let lastTodo = todoData.last {
+            return lastTodo.id + 1
+        } else {
+            return 1
+        }
+    }
 
 }
 
-#Preview {
-    AddView()
-}
+//#Preview {
+//    AddView()
+//}
--- toyProject-TodoList/ContentView.swift
+++ toyProject-TodoList/ContentView.swift
@@ -17,15 +17,23 @@ struct ContentView: View {
         NavigationStack {
             List{
                 ForEach(todoData, id: \.id) { data in
-                    NavigationLink(destination: DetailView(todo: data)) {
-                        Text(data.title)
-                            .font(.headline)
+                    HStack {
+                        Button(action: { // 버튼이 안눌러짐
+                            data.completed == true ? false : true
+                        }, label: {
+                            Image(systemName: data.completed ? "checkmark.circle.fill" : "checkmark.circle")
+                        })
+                       
+                        NavigationLink(destination: DetailView(todo: data)) {
+                            Text(data.title)
+                                .font(.headline)
+                        }
                     }
                 }
                 .onDelete(perform: delte) // 삭제 기능 삽입 (함수를 호출했는데 왜 delte()를 안해줘도 되는것인가?)
                 .onMove{ todoData.move(fromOffsets: $0, toOffset: $1) }
             }
-            .navigationBarItems(leading: EditButton(), trailing: NavigationLink(destination: AddView(), label: { Text("Add") })) // barItem 설정
+            .navigationBarItems(leading: EditButton(), trailing: NavigationLink(destination: AddView(todoData: $todoData) , label: { Text("Add") })) // barItem 설정
             .listStyle(.inset)
             .navigationTitle("Todo-List")
             .cornerRadius(10.0)
@@ -41,6 +49,11 @@ struct ContentView: View {
             todoData.remove(at: first)
         }
     }
+    
+    func toggleCompleted(at index: Int) {
+        todoData[index].completed.toggle()
+    }
+    
     
 }
 
