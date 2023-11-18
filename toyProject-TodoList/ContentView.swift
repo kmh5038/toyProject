c39e32c814be0f3bb97844135f2d31f1c7077f28
kmh5038 <144766297+kmh5038@users.noreply.github.com>
2023-11-19 01:14:58 +0900

    modify
    modify

--- toyProject-TodoList/ContentView.swift
+++ toyProject-TodoList/ContentView.swift
@@ -9,34 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var showCompleted = false
-    @State private var todoData: [Todo] = load("sample.json") // Edit모드에서 삭제 적용이 안돼서 전역변수에서 여기로 옮김
+    @State private var todoData: [Todo] = Parsing.load("sample.json") // Edit모드에서 삭제 적용이 안돼서 전역변수에서 여기로 옮김
     
     
     
     var body: some View {
         NavigationStack {
             List{
-                ForEach(todoData.indices, id: \.self) { index in
+                ForEach(todoData, id: \.id) { data in
                     HStack {
                         Button(action: { // 버튼이 안눌러짐 : row에 같은 탭 제스처를 수신하는 View 두개가 있는 경우에 이를 독립적으로 인식하지 않고 동시 작동된다. 이를 해결하기 위해서 버튼스타일을 별도로 지정해서 독립시킨다.
-                            toggleCompleted(at: index)
+                            toggleCompleted(at: data.id)
                         }
                                , label: {
-                            Image(systemName: todoData[index].completed ? "checkmark.circle.fill" : "checkmark.circle")
+                            Image(systemName: data.completed ? "checkmark.circle.fill" : "checkmark.circle")
                         })
-                        .buttonStyle(PlainButtonStyle())
+                        .buttonStyle(.plain)
                         
-                       
-                        NavigationLink(destination: DetailView(todo: todoData[index])) {
-                            Text(todoData[index].title)
+                        NavigationLink(destination: DetailView(todo: data)) {
+                            Text(data.title)
                                 .font(.headline)
                         }
-                        
-                        
                     }
-                    
                 }
-                .onDelete(perform: delte) // 삭제 기능 삽입 (함수를 호출했는데 왜 delte()를 안해줘도 되는것인가?)
+                .onDelete(perform: delete) // 삭제 기능 삽입 (함수를 호출했는데 왜 delte()를 안해줘도 되는것인가?
                 .onMove{ todoData.move(fromOffsets: $0, toOffset: $1) }
                 
             }
@@ -48,17 +44,20 @@ struct ContentView: View {
             
         }
         .padding()
+        
     }
+       
     
     
-    func delte(at offsets: IndexSet) { // 삭제 함수
+    
+    func delete(at offsets: IndexSet) { // 삭제 함수
         if let first = offsets.first {
             todoData.remove(at: first)
         }
     }
     
     func toggleCompleted(at index: Int) {
-        todoData[index].completed.toggle()
+        todoData[index-1].completed.toggle()
     }
     
     
