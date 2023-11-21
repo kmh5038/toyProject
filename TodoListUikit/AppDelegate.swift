57f43233ee0b607187143645a39e5d4108689067
kmh5038 <144766297+kmh5038@users.noreply.github.com>
2023-11-21 09:44:30 +0900

    uikit version
    uikit version

---
+++ TodoListUikit/AppDelegate.swift
@@ -0,0 +1,44 @@
+//
+//  AppDelegate.swift
+//  TodoListUikit
+//
+//  Created by 김명현 on 11/19/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        let window = UIWindow(frame: UIScreen.main.bounds)
+            let rootViewController = ViewController()
+            let navigationController = UINavigationController(rootViewController: rootViewController)
+            
+            // 윈도우에 내장된 UINavigationController를 루트 뷰 컨트롤러로 설정
+            window.rootViewController = navigationController
+            window.makeKeyAndVisible()
+            
+            return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
